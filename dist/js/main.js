ca761d8ff3417bc3a1b62994afa44a9face50463
bangvo1999 <vominhbang1999@gmail.com>
2018-11-16 19:11:56 +0700

    cập nhật lần n

--- dist/js/main.js
+++ dist/js/main.js
@@ -49,5 +49,22 @@ $(document).ready(function () {
 			}
 		}
 	});
+	$('.slider2').owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: true,
+		dots: false,
+		responsive: {
+			0: {
+				items: 2
+			},
+			600: {
+				items: 3
+			},
+			1000: {
+				items: 5
+			}
+		}
+	});
 });
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,4 +16,21 @@ $(document).ready(function () {
 			}
 		}
 	})
+	$('.slider2').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		dots:false,
+		responsive:{
+			0:{
+				items:2
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	})
 });
